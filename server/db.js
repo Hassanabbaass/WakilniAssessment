74dd21b729b51b772b0e318292eb20a14d82642f
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-20 13:11:08 +0200

    database and server setup

---
+++ server/db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'PFH#23kgrw9',
+    database:'wakilni'
+})
+
+module.exports = {db}
