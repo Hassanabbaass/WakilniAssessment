744537147926a3f727eeb1300a8d77b8ebe28efc
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-20 14:11:52 +0200

    navbar done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
+
+import '../styles.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar className='navstyle' position="static">
+                <Toolbar>
+                    <Typography className='navtitle' variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        INVENTORY
+                    </Typography>
+                    <Button className='navbtn' color="inherit">
+                        <Link className='navlink' to='/'>Product Types</Link> 
+                    </Button>
+                    <Button className='navbtn' color='inherit'>
+                        <Link className='navlink' to='/items'>Items</Link>
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    </div>
   )
 }
 
