bce048eeb8d9738d4c7173c064ddc106fe08e209
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-20 13:43:12 +0200

    react paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import ItemsPage from "./pages/ItemsPage";
+import ProductTypesPage from "./pages/ProductTypesPage";
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: '/',
+        element: <ProductTypesPage/>
+      },
+      {
+        path: '/items',
+        element: <ItemsPage/>
+      }
+    ]
+  },
+
+]);
 
 function App() {
   return (
     <div className="App">
-   
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ client/src/pages/ProductTypesPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductTypesPage = () => {
+  return (
+    <div>ProductTypesPage</div>
+  )
+}
+
+export default ProductTypesPage
